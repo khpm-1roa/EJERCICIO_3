67fa6103f9893564b2d89ab6221caebf34587305
PILLAJO-HERNAN <52302923+khpm-1roa@users.noreply.github.com>
2019-07-03 19:55:10 -0500

    Update Project Euler problema 3.js

--- Project Euler problema 3.js
+++ Project Euler problema 3.js
@@ -1,33 +1,44 @@
 //AUTOR:KEVIN PILLAJO
-//DESCRIPCION:Hacer un programa en JS que usando funciones permita resolver el problema  de lso factores de un numero 
+//DESCRIPCION:realizar el programa de euler numero3 el cual calculara los primos de un numero 
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Project Euler problema 3 </title>
 </head>
-
 <body>
-    <script>
- const long = 600851475143;
-     long = 0;
+ <h1>Project Euler problema 3
+</h1>
+  <h2>PILLAJO_KEVIN</h2>
 
-    for (long = 2; i < numm; i++) {
-// protocolo de divison      
-if (numm % i == 0) { 
-           bool = true;
-    for (long = 2; j < i; j++) {
-           if (i % j == 0) {
-           isPrime = false;
-            break;
-             }
-         }
-       if (isPrime) {
-         largestFact = i;
-       }
+</body>
+<script>
+   var final = 600851475143 ;
+   var nu =1;
+    var primos =[]
+var mul=1;
+var susecion = function (nu){
+    if(mul===final){
+        console.log("la suma de los numeros primos es :"+ mul);
+        var maximo =primos [primos.lenght - 1];
+        console.log("el mayor factor primo del numero"+final+ "es"+maximo+".");
+
+    }else{
+        if (final%nu===0){
+            div1=(nu/2);
+            if(div1%2===0 || div1%3===0){
+                return ("error");
+
+            }else{
+                console.log(nu);
+                primos.push(nu);
+                mul*=nu;
+            }
+        }
     }
- }
+}
+</SCRIPT>
+</html>
